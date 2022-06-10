7fb33709f835fe7fc24ae5b33d3b05931831b81d
kezhijie <kezhijie@wuhandsj.com>
2022-06-10 17:53:10 +0800

    权限体系调整

--- src/main/java/com/example/demolayui/controller/RoleController.java
+++ src/main/java/com/example/demolayui/controller/RoleController.java
@@ -1,8 +1,18 @@
 package com.example.demolayui.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.demolayui.entity.SysRole;
+import com.example.demolayui.service.RoleService;
+import com.example.demolayui.vo.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author kezhijie@wuhandsj.com
@@ -11,6 +21,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("role")
 public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
     @GetMapping("list")
     public String listPage() {
         return "/role/list";
@@ -22,5 +36,30 @@ public class RoleController {
     @GetMapping("edit")
     public String editPage() {
         return "/role/edit";
+    }
+
+    @GetMapping("page")
+    @ResponseBody
+    public ApiResponse<SysRole> page(@RequestParam("page") Integer page,
+                                     @RequestParam("limit") Integer limit,
+                                     @RequestParam(value = "searchParams", required = false) String searchParams) {
+        QueryWrapper<SysRole> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.hasText(searchParams)) {
+            SysRole sysRole = JSON.parseObject(searchParams, SysRole.class);
+            queryWrapper.like("name", sysRole.getName());
+        }
+
+        Page<SysRole> pageParam = new Page<>();
+        pageParam.setPages(page);
+        pageParam.setSize(limit);
+        Page<SysRole> pageUser = roleService.page(pageParam, queryWrapper);
+
+        ApiResponse<SysRole> returnData = new ApiResponse<>();
+        returnData.setCode(0);
+        returnData.setData(pageUser.getRecords());
+        returnData.setCount(pageUser.getTotal());
+        returnData.setMsg("");
+
+        return returnData;
     }
 }
